626e034bfeb332c67c9f33f60103de29277c4a01
ClashMania <113007885+ClashMania@users.noreply.github.com>
2022-09-21 09:32:45 +0200

    Add files via upload

---
+++ MojaPrvaWindowsAplikacija/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MojaPrvaWindowsAplikacija
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIzracun_Click(object sender, EventArgs e)
+        {
+            int BrojA, BrojB, Zbroj;
+            BrojA =Convert.ToInt32(txtUnosBrojaA.Text);
+            BrojB = Convert.ToInt32(txtUnosBrojaB.Text);
+            Zbroj = BrojB + BrojA;
+
+            txtIzlazBrojaA.Text = Convert.ToString(Zbroj); 
+
+
+
+
+
+
+        }
+    }
+}
